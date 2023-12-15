8077daff2c050df8b4cc6b89dc7e169301cab40c
norahtao <rt2912@columbia.edu>
2023-12-15 11:35:13 +0800

    (publish) setup.py file

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='',
+    name='sdwis_drink_water',
     version='1.0',
     packages=find_packages(),
-    description='',
+    description='Python wrappers for the Envirofacts Data Service API provided by the U.S. Environmental Protection Agency (EPA), with a focus on the Safe Drinking Water Information System (SDWIS).',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='',
-    author_email='',
-    url='',
+    author='norahtao',
+    author_email='rt2912@columbia.edu',
+    url='https://github.com/norahtao/sdwis_drink_water',
     install_requires=[
         # Any package dependencies here
+        "requests"
     ],
     classifiers=[
         # Choose classifiers: https://pypi.org/classifiers/
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ]
 )
